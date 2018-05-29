5749bbc41440658f84b3e4ea80f0201df5fa0021
k0tikov <35290162+k0tikov@users.noreply.github.com>
2018-05-29 16:53:39 +0300

    Add files via upload

--- showing/models.py
+++ showing/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
     
 class List_of_regedit_buy(models.Model):
+    TAGS_CHOICES = (
+        ('ПО', 'ПО'),
+        ('ПНР и ШМР', 'ПНР и ШМР'),
+        ('СХД', 'СХД'),
+        ('server', 'Серверное оборудование'),
+        ('network', 'Сетевое оборудование'),
+        ('support', 'Техническая поддержка'),
+        ('ibp', 'ИБП'),
+        ('consulting', 'Консалтинг')
+        )
     date_of_completion = models.IntegerField("Год договора", blank=True)
     onec_code = models.CharField("Код в 1С", max_length=50, blank=True)
     name_of_buyer = models.TextField("Наименование покупателя", blank=True)
@@ -12,7 +22,11 @@ class List_of_regedit_buy(models.Model):
     total = models.TextField("Сумма договора", blank=True)
     comments = models.TextField("Комментарии", blank=True)
     technical = models.TextField("ТехОписание", blank=True)
-    tags = models.TextField("Теги", blank=True)
+    #tags = models.TextField("Теги", blank=True)
+    tags = models.CharField(
+        max_length=1,
+        choices=TAGS_CHOICES,
+    )
     status = models.TextField("Статус", blank=True)
     subcontractor = models.TextField("Подрядчик", blank=True)
     check_number = models.TextField("№ договора с подрядчиком", blank=True)
